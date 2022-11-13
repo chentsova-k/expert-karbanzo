36f930e79a5ec7ed347e191fd9d8462c4e36139e
Karina <kosarina009@gmail.com>
2022-11-13 21:14:45 +0300

    my first commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+"use strict";
+
+
+// const result = confirm ("are you here?");
+// console.log(result);
+
+// const answer = prompt("Антон шопоголик?", "");
+// console.log(typeof(answer));
+
+const answers = [];
+
+answers [0]= prompt("как вас зовут", "");
+answers [1]= prompt("как дела", "");
+answers [2]= prompt("когда гулять", "");
+
+document.write(answers);
+
+
+
+
+
